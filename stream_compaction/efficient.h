b913fc07ce11204dccd5a0b2c845530bf1345782
z <1090589429@qq.com>
2015-09-13 19:25:01 -0400

    efficient

--- stream_compaction/efficient.h
+++ stream_compaction/efficient.h
@@ -3,7 +3,7 @@
 namespace StreamCompaction {
 namespace Efficient {
     void scan(int n, int *odata, const int *idata);
-
+	void init(int n,const int*b);
     int compact(int n, int *odata, const int *idata);
 }
 }

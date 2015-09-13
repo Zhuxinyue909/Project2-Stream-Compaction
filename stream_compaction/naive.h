904b9aa4e201a99d580ea11e719e9c465ccad67f
z <1090589429@qq.com>
2015-09-13 19:25:22 -0400

    naive

--- stream_compaction/naive.h
+++ stream_compaction/naive.h
@@ -3,5 +3,8 @@
 namespace StreamCompaction {
 namespace Naive {
     void scan(int n, int *odata, const int *idata);
+	void init(int *hst_idata,int *hst_odata,int n);
+	//int *dev_A;
+	//int *dev_B;
 }
 }

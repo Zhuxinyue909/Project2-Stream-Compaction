45d1f2ed0fe5d5c8b3d8d1946c8d8da1a982d4a6
z <1090589429@qq.com>
2015-09-13 19:25:51 -0400

    thrust function

--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -16,7 +16,24 @@ void scan(int n, int *odata, const int *idata) {
     // TODO use `thrust::exclusive_scan`
     // example: for device_vectors dv_in and dv_out:
     // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+
+	thrust::device_vector<int> dv_in,dv_out;
+	for (int i = 0; i < n; i++){
+		dv_in.push_back(idata[i]);
+		dv_out.push_back(0);
+	}
+	thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+	thrust::copy(dv_out.begin(), dv_out.end(), odata);
+
+
+	printf("4.1");
+
+		
+}
+
+
+
 }
 
 }
-}
+
